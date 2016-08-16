a8ff6a5a5b618dc98cd093e7a1f176c0f8d60669
sammyclark <iamsam10@hotmail.co.uk>
2016-08-16 14:10:51 +0100

    started simple calculator mini project

---
+++ 1. Language Basics/MiniProject_Calculator/MiniProject_Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decimal num1;
+            Decimal num2;
+            string sign;
+
+            //created variables for number values and signs
+
+            Console.WriteLine("Please enter a number");
+            num1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            num2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please enter a sign (+ , - , x , /)");
+            sign = Console.ReadLine();
+
+            //created inputs for number values and signs
+
+             
+         
+        }
+    }
+}

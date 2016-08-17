a70f96775fe70c897ca84f6b9892e0542d68f200
sammyclark <iamsam10@hotmail.co.uk>
2016-08-17 15:25:10 +0100

    Slides

---
+++ 2. Object Orientation/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Car
+    {
+        public virtual void
+            {
+               Console.WriteLine(); 
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
